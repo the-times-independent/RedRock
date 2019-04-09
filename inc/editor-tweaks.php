2c845d689064d0cb7d2c81ec54609c37b0091190
Carter Pape <carterpape@Carters-MacBook-Pro.local>
2019-04-09 10:41:51 -0600

    filters titles for whitespace problems, too

--- inc/editor-tweaks.php
+++ inc/editor-tweaks.php
@@ -1,14 +1,28 @@
 <?php
 
-function redrock_remove_erroneous_whitespace($text) {
-    $text = str_replace('&nbsp;', ' ', $text);
-    $text = preg_replace('/^<p>\s+/m', '<p>', $text);
-    $text = preg_replace('/\s+<\/p>$/m', '</p>', $text);
-    $text = preg_replace('/\h\h+/', ' ', $text);
-    return $text;
+function redrock_remove_erroneous_content_whitespace($content) {
+    $content = str_replace('&nbsp;', ' ', $content);
+    $content = preg_replace('/^<p>\h+/m', '<p>', $content);
+    $content = preg_replace('/\h+<\/p>$/m', '</p>', $content);
+    $content = preg_replace('/\h\h+/', ' ', $content);
+    return $content;
 }
 
 function redrock_filter_content($content) {
-    return redrock_remove_erroneous_whitespace($content);
+    return redrock_remove_erroneous_content_whitespace($content);
 }
 add_filter('content_save_pre','redrock_filter_content');
+
+
+function redrock_remove_erroneous_title_whitespace($text) {
+    $text = preg_replace('/ *&nbsp; */', '&nbsp;', $text);
+    $text = preg_replace('/^\s+/m', '', $text);
+    $text = preg_replace('/\s+$/m', '', $text);
+    $text = preg_replace('/\s\s+/', ' ', $text);
+    return $text;
+}
+
+function redrock_filter_title($title) {
+    return redrock_remove_erroneous_title_whitespace($title);
+}
+add_filter('title_save_pre','redrock_filter_title');
