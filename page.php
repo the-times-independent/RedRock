6f49dbdc50c5c8366ad1778ab0ca24f70f6528b9
Carter Pape <carterpape@Carters-MacBook-Pro.local>
2019-04-11 16:06:09 -0600

    why did I miss the weird page formatting

--- page.php
+++ page.php
@@ -20,13 +20,9 @@ get_header();
 <?php
         while (have_posts()) {
             the_post();
-?>
-
-<?php
+            
             get_template_part('template-parts/content', 'page');
-?>
-
-<?php
+            
             if (comments_open() || get_comments_number()) {
                 comments_template();
             }
