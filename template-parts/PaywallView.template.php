9346729be77a063b9684851f0dec6476405bfd57
Carter Pape <carterpape@gmail.com>
2019-12-30 16:15:33 -0700

    prefer references to classes in CSS, not IDs

--- template-parts/PaywallView.template.php
+++ template-parts/PaywallView.template.php
@@ -4,12 +4,15 @@ namespace RedRock;
 
 ?>
 
-<div id="paywall">
+<div id="paywall" class="paywall">
     <div class="content-width">
-        <h1>You are out of free articles.</h1>
+        <h1>You are out of free articles for this month.</h1>
         <p>
             The good news is that you don't have to stop reading.
             Unlimited access to The-Times Independent starts at just <strong>$1 per week</strong>.
+            <br />
+            The T-I is proudly owned and operated in Moab, Utah.
+            A subscription to The Times-Independent supports the area's oldest, most historied journalism institution.
         </p>
 <?php
         $subscriptionListView = new SubscriptionListView();
--- template-parts/SubscriptionPlanListView.template.php
+++ template-parts/SubscriptionPlanListView.template.php
@@ -4,36 +4,37 @@ namespace RedRock;
 
 ?>
 
-<div id="print-plus-digital-offer">
+<div class="print-plus-digital-offer">
     <h1>Print + Digital</h1>
     <div class="offer-details">
         <h2>Full access</h2>
         <ul>
             <li>Read unlimited articles from any device.</li>
-            <li>Get the Print Edition delivered to your address every week of the year.</li>
+            <li>Get the Print Edition delivered to your address every week of the year. <strong class="free-delivery-message">Delivery is free in and around Moab.</strong></li>
         </ul>
         <h2>Early access</h2>
         <ul>
             <li>Get previews of new media releases, including apps, newsletters, podcasts and more.</li>
         </ul>
         <div class="price-details">
-            <a class="button" href="<?php  ?>">Proceed to checkout</a>
+            <form class="zip-code-form zip-info-to-be-replaced">
+                <input type="text" class="zip-code-input" placeholder="5-digit zip code" />
+                <input type="submit" class="zip-code-submit" value="Calculate shipping" />
+                <p class="zip-form-error-text error-text hidden">Please enter a valid zip code.</p>
+            </form>
             <div class="offer-price">
-                <span class="currency">$</span><span class="digit">4</span>
+                <span class="currency">$</span>
+                <span class="digit">4</span>
                 <span class="frequency-slash" />
                 <span class="frequency">four weeks</span>
+                <span class="shipping-cost"> + shipping</span>
             </div>
-            <form id="zip-code-form" class="zip-info-to-be-replaced">
-                <p>Check shipping cost:</p>
-                <input type="text" id="zip-code-input" placeholder="5-digit zip code" />
-                <input type="submit" id="zip-code-submit" value="Go" />
-                <p id="zip-form-error-text" class="error-text hidden">Please enter a valid zip code.</p>
-            </form>
+            <a class="button" href="<?php  ?>">Proceed to checkout</a>
         <div>
     </div>
 </div>
 
-<div id="digital-offer">
+<div class="digital-offer">
     <h1>Digital</h1>
     <div class="offer-details">
         <h2>Unlimited online access</h2>
@@ -45,34 +46,37 @@ namespace RedRock;
             <li>Get previews of new media releases, including apps, newsletters, podcasts and more.</li>
         </ul>
         <div class="price-details">
-            <a class="button" ref="<?php  ?>">Proceed to checkout</a>
             <div class="offer-price">
-                <span class="currency">$</span><span class="digit">4</span>
+                <span class="currency">$</span>
+                <span class="digit">4</span>
                 <span class="frequency-slash" />
                 <span class="frequency">four weeks</span>
             </div>
+            <a class="button" ref="<?php  ?>">Proceed to checkout</a>
         <div>
     </div>
 </div>
 
-<div id="auxillary-offers">
-    <div id="print-offer">
+<div class="auxillary-offers">
+    <div class="offer-for-current-subscribers">
+        <h1>Current subscribers</h1>
+        <p>
+            Already receiving the Print Edition? Gain digital access today.
+        </p>
+        <a class="button" href="<?php  ?>">Link existing subscription</a>
+    </div>
+    <div class="print-offer">
         <h1>Print-only subscriptions</h1>
         <p>
             Don't care for digital access? Get the Print Edition delivered to you weekly.
         </p>
         <a class="button" href="<?php  ?>">Contact us</a>
     </div>
-    <div id="gift-offer">
+    <div class="gift-offer">
         <h1>Gift a subscription</h1>
         <p>
-            Support Moab's oldest, most historied journalism institution. Give the gift of a subscription to The Times-Independent.
+            A subscription to The Times-Independent is a gift that keeps on giving—every week of the year.
         </p>
         <a class="button" href="<?php  ?>">See gift offers</a>
-    </div>
-    <div id="offer-for-current-subscribers">
-        <h1>Current subscribers</h1>
-        <p>Already receiving the Print Edition? Gain digital access today.</p>
-        <a class="button" href="<?php  ?>">Link existing subscription</a>
     </div>
 </div>
